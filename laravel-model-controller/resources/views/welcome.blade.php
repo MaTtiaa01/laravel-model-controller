747781b2e03f4c916f4384df987fb56ba3288913
MaTtiaa01 <mattia2001@live.it>
2022-12-19 15:04:46 +0100

    print movie card

--- laravel-model-controller/resources/views/welcome.blade.php
+++ laravel-model-controller/resources/views/welcome.blade.php
@@ -2,6 +2,26 @@
 
 @section('content')
 
+<div class="container">
+    <div class="row row-cols-2 row-cols-md-4">
+        @forelse($movies as $movie)
+        <div class="col">
+            <div class="card p-2 text-center">
+                <div class="card-head">
+                    <div> Title:{{$movie -> title}} </div>
+                    <span> Original Title: {{$movie -> original_title}}</span>
+                </div>
+                <hr>
+                <div class="card-body">
+                    <div class="vote">Vote: {{$movie->vote}}</div>
+                    <div class="nationality">Nationality: {{$movie->nationality}}</div>
+                </div>
+            </div>
+        </div>
+        @empty
 
-<h1>welcome</h1>
+        @endforelse
+    </div>
+</div>
+
 @endsection
